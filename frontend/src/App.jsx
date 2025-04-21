2c04867a7465617f1e290bc53b9065f058d66ce5
Manuel Cornejo <manuelxcornejo@gmail.com>
2025-04-21 19:43:23 -0400

    feat: se agrega configuracion para bootstrap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,13 @@
-
-
-import './App.css'
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-
-
   return (
     <>
-    
+      <h1 className="text-green">Hola mundo</h1>
+      <button className="btn btn-primary">Este es el boton numero 1</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
