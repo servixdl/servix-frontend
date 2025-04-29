e094894a554b4f9c61c353744aa28f9c56de34a2
Manuel Cornejo <manuelxcornejo@gmail.com>
2025-04-28 23:51:41 -0400

    feat:add components for test tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
 import { Route, Routes } from "react-router-dom";
-import "./App.css";
 import Home from "./feature/home/contains/Home.jsx";
 import ServicePage from "./feature/services/contains/Service.jsx";
 import AllServicePage from "./feature/services/contains/AllServices.jsx";
 import LoginPage from "./feature/user/contains/Login.jsx";
 import RegisterPage from "./feature/user/contains/Register.jsx";
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/service" element={<ServicePage />} />
-        <Route path="/allservice" element={<AllServicePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="*" element={<h1>404 Not Found</h1>} />
-      </Routes>
+      <>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/service" element={<ServicePage />} />
+          <Route path="/allservice" element={<AllServicePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="*" element={<h1>404 Not Found</h1>} />
+        </Routes>
+        <Footer />
+      </>
     </div>
   );
 }
