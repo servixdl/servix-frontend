d055ee9fa793d84723dde1ec90e5c00e52272785
cristian Di d Silv0 <a.diaz.1987.08@hotmail.com>
2025-05-09 09:27:36 -0400

    profile rendering completed

--- src/config/constans.js
+++ src/config/constans.js
@@ -3,5 +3,5 @@ export const URLBASE = 'http://localhost:3000'
 export const ENDPOINT = {
   login: `${URLBASE}/users/login`,
   registrer: `${URLBASE}/users`,
-   users : `${URLBASE}/users`
+   perfil : `${URLBASE}/users`
 }
