0b10d4a111ae2025b20c935811a35d077656b9c9
Manuel Cornejo <manuelxcornejo@gmail.com>
2025-05-11 01:08:39 -0400

    feat: add ContactPage component and update Navigation routes

--- src/routes/Navigation.jsx
+++ src/routes/Navigation.jsx
@@ -1,39 +1,40 @@
-import {  BrowserRouter as Router,Routes,Route } from "react-router-dom";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Home from "../feature/home/contains/Home.jsx";
 import ServicePage from "../feature/services/contains/Service.jsx";
 import AllServicePage from "../feature/services/contains/AllServices.jsx";
 import RegisterPage from "../feature/user/contains/Register.jsx";
 import LoginPage from "../feature/user/contains/Login.jsx";
-import PerfilUsuario from "../feature/user/contains/ProfileUser.jsx"; 
+import PerfilUsuario from "../feature/user/contains/ProfileUser.jsx";
 import PrivateRoute from "../components/PrivateRoute.jsx";
 import NotFound from "../feature/notFound/NotFound.jsx";
 import Sale from "../feature/sale/content/Sale.jsx";
 import MyServices from "../feature/myServices/contens/MyServices.jsx";
 import { useContext } from "react";
 import { TokenContext } from "../context/tokenContext.jsx";
+import ContactPage from "../feature/contact/Contact.jsx";
 function Navigation() {
-    const {token} = useContext(TokenContext);
+  const { token } = useContext(TokenContext);
   return (
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/service/:id" element={<ServicePage />} />
-          <Route path="/allservice" element={<AllServicePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/sale/:id" element={!token? <Home/> :< Sale/>} />
-          <Route path="/myServices" element={!token? <Home/>:<MyServices/>}></Route>
-          <Route
-            path="/perfil"
-            element={
-              <PrivateRoute>
-                <PerfilUsuario />
-              </PrivateRoute>
-            }
-          />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
- 
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/service/:id" element={<ServicePage />} />
+      <Route path="/allservice" element={<AllServicePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/sale/:id" element={!token ? <Home /> : <Sale />} />
+      <Route path="/myServices" element={!token ? <Home /> : <MyServices />} />
+      <Route path="/contact" element={<ContactPage />} />
+      <Route
+        path="/perfil"
+        element={
+          <PrivateRoute>
+            <PerfilUsuario />
+          </PrivateRoute>
+        }
+      />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
